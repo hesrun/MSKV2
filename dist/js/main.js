32807edfe2f23453e52c196ecd3ff8394462a11a
Vladyslav Otroshchenko <vladmih1988@gmail.com>
2024-07-09 21:40:42 +0200

    new sections index page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,16 @@
 "use strict";
 
-//openMap
+var swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.reviews-next',
+    prevEl: '.reviews-prev'
+  }
+}); //faq
+
+$('.faq__link').click(function () {
+  $(this).next().slideToggle(300);
+}); //openMap
+
 $('.openMap').click(function () {
   $('#modalMap').modal({
     fadeDuration: 300
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
+var swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.reviews-next',
+        prevEl: '.reviews-prev',
+    },
+})
+
+//faq
+$('.faq__link').click(function () {
+    $(this).next().slideToggle(300)
+})
 //openMap
 $('.openMap').click(function () {
     $('#modalMap').modal({
